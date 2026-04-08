97fd1da0a229f9fc4f9488bcfb0edcbfb3041071
Sahil Ansari <sahillansarii@gmail.com>
2026-04-08 15:47:56 +0530

    style: format import statements for better readability

--- trackyBackend/src/Routes/task.routes.js
+++ trackyBackend/src/Routes/task.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { createTask, deleteTask, getAllTask, getTask, updateTask } from "../Controllers/task.controller.js";
+import {
+  createTask,
+  deleteTask,
+  getAllTask,
+  getTask,
+  updateTask,
+} from "../Controllers/task.controller.js";
 
 const router = express.Router();
 
